7cd3d8e66a0dcc91b5ac5da8a626ab398d9d4d54
minadarvishi <mina.darvishi1379@gmail.com>
2025-09-18 21:36:50 +0330

    database for render

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,session,url_for,request,redirect,flash
+from flask import Flask, render_template,session
 from extensions import db
 from admin.routes import admin_bp
 from main.routes import main_bp
@@ -7,29 +7,31 @@ import os
 
 def create_app():
     app = Flask(__name__)
-
-    # --- پیکربندی پایگاه داده ---
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'bookstore.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # --- اتصال افزونه‌ها به اپلیکیشن ---
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    # --- افزودن کلید مخفی برای امنیت سشن ---
     app.config['SECRET_KEY'] = 'a-super-secret-key-that-no-one-can-guess'
+
+    # --- اتصال هوشمند به دیتابیس ---
+    db_url = os.environ.get('DATABASE_URL')
+    if not db_url:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        db_url = 'sqlite:///' + os.path.join(basedir, 'bookstore.db')
+        print("--- RUNNING ON LOCAL SQLITE DB ---")
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    # ------------------------------------
 
     # --- ثبت کردن Blueprintها ---
     app.register_blueprint(main_bp)
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(cart_bp)
 
-    # ساخت دیتابیس
+    #  ساخت دیتابیس
     @app.cli.command('init-db')
     def init_db_command():
         db.create_all()
         print('Initialized the database and created all tables.')
-
 
     @app.context_processor
     def cart_count():
@@ -41,11 +43,8 @@ def create_app():
     def not_found_error(error):
         return render_template('404.html'), 404
 
-
-
-
-
     return app
+
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
